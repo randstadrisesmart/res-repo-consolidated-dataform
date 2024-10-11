25fd90a15d46bb8c83fca82baf4959b12877cc0c
SapnaB <sapna.brahmbhatt@randstadsourceright.com>
2024-10-11 11:18:37 -0400

    changed starttime variable from const to var in declarations file

--- includes/declarations.js
+++ includes/declarations.js
@@ -2,7 +2,7 @@
 var date = new Date();
 var current_date = date.getFullYear()+"-"+(date.getMonth()+1)+"-"+ date.getDate();
 var current_time = date.getHours()+":"+date.getMinutes()+":"+ date.getSeconds();
-const starttime = "'" + current_date + " " + current_time + "'";
+var starttime = "'" + current_date + " " + current_time + "'";
 //const starttime = `CURRENT_TIMESTAMP()`;
 const dfm_name = "'dataform-blue-cnsd-nam'"; // update this when pull updated code from another region form
 
